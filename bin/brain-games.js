77f3c659c7f09a092c1198283915e638cf09d248
olga <olgatreushkova@gmail.com>
2024-08-26 18:00:49 +0300

    добавлены файлы

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
+const greetUser = require('..src/cli');
 console.log('Welcome to the Brain Games!');
 
+greetUser();
 
--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,5 @@ const greetUser = () => {
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? '); 
 console.log(`Hello, ${userName}!`);
- };
+};
+module.exports = greetUser;
