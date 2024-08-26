fb5e8edf36b5bc0af9cf3760c1abceab90ed4d50
olga <olgatreushkova@gmail.com>
2024-08-26 17:52:01 +0300

    добавлены файлы

--- brain-games.js
+++ brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+ const greetUser = require('..src/cli');
 
+greetUser(); 
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+const readlineSync = require('readline-sync');
+const greetUser = () => { 
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? '); 
+console.log(`Hello, ${userName}!`);
+ };
