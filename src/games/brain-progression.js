d35e79fdbc259dbbbe51d10bdfc4819d922bc29f
olga <olgatreushkova@gmail.com>
2024-09-01 23:41:36 +0300

    something changes

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,52 +1,22 @@
-import readlineSync from 'readline-sync';
+import { getNumber, start } from '../index.js';
 
-const playRound =() => {
+const result = () => {
+  const progression = [];
+  const first = getNumber();
+  for (let i = first; i < first + 10; i += 1) {
+    progression.push(i);
+  }
+  const currentIndex = Math.floor(Math.random() * 10);
+  const missingIndex = progression[currentIndex];
+  progression[currentIndex] = '..';
+  const question = `Question: ${progression.join(' ')}`;
+  const correct = missingIndex.toString();
+  return [question, correct];
+};
 
-    const length =  Math.floor(Math.random() * 6) + 5;
-       const first = Math.floor(Math.random() * 10) + 1;
-           const step = Math.floor(Math.random() * 10) + 1;
-          
-           const progression = [];    
-           
-           for (let i = 0; i < length; i+=1) {
-               const currentNumber = first + i * step; 
-           progression.push(currentNumber);
-           }
-   
-           const hiddenIndex = Math.floor(Math.random() * length);
-           const hiddenNumber = progression[hiddenIndex];
-           progression[hiddenIndex] = '..';
-   
-           console.log(`Question: ${progression.join(' ')}`);
-            const userAnswer = readlineSync.question('Your answer: ');
-   
-               if (Number(userAnswer) === hiddenNumber) {
-                   console.log('Correct!');
-                   return true;
-               
-           } else {
-               console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${hiddenNumber}.`);
-               return false;
-           }
-       }
-   
-const proGame = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log('What number is missing in the progression?');
-  
-const rounds = 3;
-    for (let i = 0; i < rounds; i+=1) {
-        
-        const isCorrect = playRound();
-        if (!isCorrect){
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
-        }
-    
-        console.log(`Congratulations, ${userName}!`);
-    };
-    
+const game = () => {
+  const newMessage = 'What number is missing in the progression?';
+  start(newMessage, result);
+};
 
-export default proGame;
+export default game;
