3f64473cf7965a0508c44c95409771f4a0e75662
olga <olgatreushkova@gmail.com>
2024-09-01 23:05:24 +0300

    something changes

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,50 +1,52 @@
 import readlineSync from 'readline-sync';
 
-function proGame() {
-    const userName = readlineSync.question('May I have your name? ');
- console.log(`Hello, ${userName}!`);
-    console.log("What number is missing in the progression?");
-    
-    for (let i = 0; i < 3; i++) {
+const playRound =() => {
+
+    const length =  Math.floor(Math.random() * 6) + 5;
+       const first = Math.floor(Math.random() * 10) + 1;
+           const step = Math.floor(Math.random() * 10) + 1;
+          
+           const progression = [];    
+           
+           for (let i = 0; i < length; i+=1) {
+               const currentNumber = first + i * step; 
+           progression.push(currentNumber);
+           }
+   
+           const hiddenIndex = Math.floor(Math.random() * length);
+           const hiddenNumber = progression[hiddenIndex];
+           progression[hiddenIndex] = '..';
+   
+           console.log(`Question: ${progression.join(' ')}`);
+            const userAnswer = readlineSync.question('Your answer: ');
+   
+               if (Number(userAnswer) === hiddenNumber) {
+                   console.log('Correct!');
+                   return true;
+               
+           } else {
+               console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${hiddenNumber}.`);
+               return false;
+           }
+       }
+   
+const proGame = () => {
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('What number is missing in the progression?');
+  
+const rounds = 3;
+    for (let i = 0; i < rounds; i+=1) {
         
-        const start = Math.floor(Math.random() * 10) + 1;
-        const step = Math.floor(Math.random() * 10) + 1;
-        const length = 10;
-
-        let progression = "";
-        let hiddenNumber = null;
-              
-        const hiddenIndex = Math.floor(Math.random() * length);
-
-        for (let j = 0; j < length; j++) {
-            const currentNumber = start + j * step;
-            if (j === hiddenIndex) {
-                progression += '.. '; 
-                hiddenNumber = currentNumber; 
-
-            } else {
-                progression += currentNumber + ' ';  
-            }
-        }
-
-        
-        console.log(`Question: ${progression.trim()}`);
-        
-       
-        const userAnswer = parseInt(("Your answer:"), 10);
-        
-     
-        if (userAnswer === hiddenNumber) {
-            console.log("Correct!");
-
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${hiddenNumber}'.`);
+        const isCorrect = playRound();
+        if (!isCorrect){
             console.log(`Let's try again, ${userName}!`);
             return;
         }
-    }
+        }
     
-    console.log(`Congratulations, ${userName}!`);
-}
+        console.log(`Congratulations, ${userName}!`);
+    };
+    
 
-proGame();
+export default proGame;
