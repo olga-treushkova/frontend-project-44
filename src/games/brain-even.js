4bc10e683fe691588b348a7d3761ef9cf27a5feb
olga <olgatreushkova@gmail.com>
2024-09-02 15:50:09 +0300

    changes

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 function startGame() {
 
   console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
