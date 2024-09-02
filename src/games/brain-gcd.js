582fbdcea34bfdf220fb643b6e5eda8fe7bc3a1c
olga <olgatreushkova@gmail.com>
2024-09-03 01:25:48 +0300

    changes

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -10,7 +10,7 @@ const gcd = (a, b) => {
 const getRandom = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const runGame = () => {
-  console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Find the greatest common divisor of given numbers.');
@@ -35,7 +35,7 @@ const runGame = () => {
     }
   }
 
-  console.log(`Congratulations, ${userName}!`);
-};
+    console.log(`Congratulations, ${userName}!`);
+    };
 
 export default runGame;
