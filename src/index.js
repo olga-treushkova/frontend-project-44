356e140316416aa7e7168fdd12d43eec02e20e76
olga <olgatreushkova@gmail.com>
2024-09-03 22:07:00 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ function getName() {
 }
 function getAnswer() {
   const number = Math.floor(Math.random() * 100);
+  const userName = readlineSync.question('May I have your name? ');
   const correctAnswer = (number % 2 === 0);
   const userAnswer = readlineSync.question('Your answer: ');
   if (userAnswer !== correctAnswer) {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
     return;
   }
   console.log('Correct!');
