126d9249d44b4cec02313b22f88b69a03eb03943
olga <olgatreushkova@gmail.com>
2024-09-06 00:52:43 +0300

    new changesin files

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,33 +1,27 @@
-import readlineSync from 'readline-sync';
 import gameLogic from '../index.js';
+import { getRandomIndex } from '../randomNumber.js';
 
-const playRound = () => {
-  const length = Math.floor(Math.random() * 6) + 5;
-  const first = Math.floor(Math.random() * 10) + 1;
-  const step = Math.floor(Math.random() * 10) + 1;
+const task = 'What number is missing in the progression?';
 
-  const progression = [];
-  for (let i = 0; i < length; i += 1) {
+const getRound = () => {
+const [ length, first, step ] = getRandomIndex();
+
+const progression = [];
+for (let i = 0; i < length; i += 1) {
     const currentNumber = first + i * step;
     progression.push(currentNumber);
-  }
-  const hiddenIndex = Math.floor(Math.random() * length);
-  const hiddenNumber = progression[hiddenIndex];
-  progression[hiddenIndex] = '..';
+}
 
-  console.log(`Question: ${progression.join(' ')}`);
-  const userAnswer = Number(readlineSync.question('Your answer: '));
+const hiddenIndex = Math.floor(Math.random() * length);
+const hiddenNumber = progression[hiddenIndex];
+progression[hiddenIndex] = '..';
 
-  if (userAnswer === hiddenNumber) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${hiddenNumber}.`);
-  return false;
+const question = `${progression.join(' ')}`;
+return [question, String(hiddenNumber)];
 };
 
 const proGame = () => {
-  gameLogic();
+  gameLogic(task, getRound);
 };
 
 export default proGame;
--- src/randomNumber.js
+++ src/randomNumber.js
@@ -6,4 +6,10 @@ const getRandom = () => {
 };
 export default getRandom;
 
-
+const getRandomIndex = () => {
+    const length = Math.floor(Math.random() * 6) + 5;
+    const first = Math.floor(Math.random() * 10) + 1;
+    const step = Math.floor(Math.random() * 10) + 1;
+    return [length, first, step];
+};
+export { getRandomIndex };
