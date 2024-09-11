de85407b7b23ede4be14fec06ad74fcf4b774f18
olga <olgatreushkova@gmail.com>
2024-09-11 11:55:15 +0300

    changes

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,5 +1,5 @@
 import startGame from '../index.js';
-import getRandom from '../helpers.js';
+import randomNumber from '../helpers.js';
 
 const task = 'What is the result of the expression?';
 const min = 0;
@@ -7,8 +7,8 @@ const max = 100;
 
 const getRandomOperation = () => {
   const operations = ['+', '-', '*'];
-  const randomNumber = Math.floor(Math.random() * operations.length);
-  return operations[randomNumber];
+  const randomNum = Math.floor(Math.random() * operations.length);
+  return operations[randomNum];
 };
 
 const calc = (number1, operation, number2) => {
@@ -25,8 +25,8 @@ const calc = (number1, operation, number2) => {
 };
 
 const getRound = () => {
-  const number1 = getRandom(min, max);
-  const number2 = getRandom(min, max);
+  const number1 = randomNumber(min, max);
+  const number2 = randomNumber(min, max);
   const operation = getRandomOperation();
   const question = `${number1} ${operation} ${number2}`;
   const correctAnswer = String(calc(number1, operation, number2));
